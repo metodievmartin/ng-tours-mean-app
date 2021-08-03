950ab0ebdec4025f4a6e66797a40802555be9ca7
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-03 17:15:46 +0300

    feat(module): set up tours module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { RouterModule } from '@angular/router';
+import { ToursModule } from './tours/tours.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CoreModule,
+    ToursModule,
     RouterModule,
     AppRoutingModule
   ],
