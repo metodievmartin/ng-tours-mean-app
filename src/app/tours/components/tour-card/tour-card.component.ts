265ee44c45610cde6e171e2cc308482cf52fcdac
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-03 17:49:52 +0300

    feat(component): create tour-card
    Create the component with styling and static HTML. Represents a single
    tour with all the essential information as a card

---
+++ src/app/tours/components/tour-card/tour-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-card',
+  templateUrl: './tour-card.component.html',
+  styleUrls: ['./tour-card.component.css']
+})
+export class TourCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
