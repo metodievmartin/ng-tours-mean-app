076480bbe65912229a6634e422fbcb50e8030a2c
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-04 11:15:00 +0300

    feat(module): set up auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { RouterModule } from '@angular/router';
 import { ToursModule } from './tours/tours.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CoreModule,
+    AuthModule,
     ToursModule,
     RouterModule,
     AppRoutingModule
