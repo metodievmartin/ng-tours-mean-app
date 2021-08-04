be92f3e4e6eb515804a61f94befce6fd078ef5bd
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-04 16:41:39 +0300

    feat(module): set up users module
    This module will contain all the user related functionality (except auth logic)
    such as: user profile, account settings, user info updates, manage user bookings & reviews, etc.

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+    declarations: [
+    ],
+    exports: [
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class UsersModule { }
