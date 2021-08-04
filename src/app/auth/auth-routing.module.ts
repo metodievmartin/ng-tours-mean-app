f797a81954e13bfa2c1881575751b7a95936e30f
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-04 15:48:52 +0300

    feat(routing): add register-page to auth routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { NgModule } from '@angular/core';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 
 const authRoutes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
 ];
 
 @NgModule({
