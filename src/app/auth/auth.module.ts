f968c256d6368c1a78629978b9323e8f6998c9b6
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-04 15:46:15 +0300

    feat(component): create register-page
    Create the component with styling and static content. Forms control
    & functionality yet to be added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   exports: [
     LoginPageComponent
