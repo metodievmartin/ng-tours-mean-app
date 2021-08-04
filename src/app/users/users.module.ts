98e29982e4ae06b82a6982596d33e78a009a1c56
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-04 16:45:47 +0300

    feat(component): create user-profile
    Create the component with styling and static content. Serves as a dashboard
    where users can manage their data, activities & etc.
    Needs to be broken down to smaller components

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 
 
 @NgModule({
     declarations: [
+        UserProfileComponent
     ],
     exports: [
+        UserProfileComponent
     ],
     imports: [
         CommonModule
