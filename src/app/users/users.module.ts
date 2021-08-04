b6261693e86c43f087b500437dd388f0236931f5
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-04 19:37:16 +0300

    refactor(component): extract user-settings component
    Extract the user account settings section from user-profile component
    into a new separate component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,13 +4,15 @@ import { UserProfileComponent } from './components/user-profile/user-profile.com
 import { UsersRoutingModule } from './users-routing.module';
 import { SideNavItemComponent } from './components/user-profile/side-nav/side-nav-item/side-nav-item.component';
 import { SideNavComponent } from './components/user-profile/side-nav/side-nav.component';
+import { UserSettingsComponent } from './components/user-profile/user-settings/user-settings.component';
 
 
 @NgModule({
   declarations: [
     UserProfileComponent,
     SideNavComponent,
-    SideNavItemComponent
+    SideNavItemComponent,
+    UserSettingsComponent
   ],
   exports: [
     UserProfileComponent
