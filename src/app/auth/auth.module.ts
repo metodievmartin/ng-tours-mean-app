389c835945c3ddf96cb973522a59a9f815702f46
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-04 11:55:12 +0300

    feat(component): create login-page
    Create the component with styling and static content. Forms control
    & functionality yet to be added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent
+  ],
+  exports: [
+    LoginPageComponent
+  ],
   imports: [
     CommonModule
   ]
