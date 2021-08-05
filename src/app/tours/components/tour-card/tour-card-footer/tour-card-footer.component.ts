7d73edea36407e496bc3dcab9b4adf1a84fe097c
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-05 12:59:08 +0300

    refactor(component): extract 3 new components from tour-card
    Break down tour-card component into 3 smaller & more compact components:
    tour-card-header, tour-card-details & tour-card-footer

---
+++ src/app/tours/components/tour-card/tour-card-footer/tour-card-footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-card-footer',
+  templateUrl: './tour-card-footer.component.html',
+  styleUrls: ['./tour-card-footer.component.css']
+})
+export class TourCardFooterComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/tours/components/tour-card/tour-card-header/tour-card-header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-card-header',
+  templateUrl: './tour-card-header.component.html',
+  styleUrls: ['./tour-card-header.component.css'],
+  host: { 'class': 'card-header' }
+})
+export class TourCardHeaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
