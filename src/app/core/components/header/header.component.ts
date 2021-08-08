4ec38b9059ef6e9653f51ff50aa31aa6c1cdaa52
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-08 11:38:46 +0300

    feat(component): implement header nav conditional rendering
    Header's user and guest navigation conditionally rendered based auth state

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 
 import * as fromApp from '../../../reducers'
 import { AuthActions } from '../../../auth/store/actions';
+import { environment } from '../../../../environments/environment';
+import { User } from '../../../auth/interfaces';
 
 @Component({
   selector: 'app-header',
@@ -10,12 +12,20 @@ import { AuthActions } from '../../../auth/store/actions';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  urlBase = environment.restApiHost + environment.userImg;
+  user: User | null = null;
+  isAuthenticated: boolean = false;
 
   constructor(
     public store: Store<fromApp.AppState>
   ) { }
 
   ngOnInit(): void {
+    this.store.select(state => state.auth)
+      .subscribe(authState => {
+        this.user = authState.user;
+        this.isAuthenticated = authState.isAuthenticated;
+      });
   }
 
   onLogout() {
