b8cb184f0afdb2b6f63b3ff78cc86ef1a97f3df8
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-12 11:29:10 +0300

    refactor(component): pass title & subtitle as input values to banner

--- src/app/tours/components/banner/banner.component.ts
+++ src/app/tours/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
+  @Input() title = '';
+  @Input() subtitle = '';
 
   constructor() {
   }
