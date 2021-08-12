b49c96fcaf710ea39f1b0251b31c8a643ec7c0bb
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-12 12:06:13 +0300

    refactor: add my-tours endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   signupUrl: '/api/v1/users/signup/',
   loginUrl: '/api/v1/users/login/',
   allToursUrl: '/api/v1/tours/',
+  myToursUrl: '/api/v1/tours/booked',
   topFiveToursUrl: '/api/v1/tours/top-5-cheap',
   oneTourBySlugUrl: '/api/v1/tours/slug/',
   allBookingsUrl: '/api/v1/bookings/',
