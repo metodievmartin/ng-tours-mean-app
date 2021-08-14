b93a2a302f0d929455e5192076fa098e0aee1950
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-14 19:50:21 +0300

    refactor: unsubscribe from the store OnDestroy

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 import * as fromApp from '../../../reducers'
 import { AuthActions } from '../../../auth/store/actions';
 import { environment } from '../../../../environments/environment';
 import { User } from '../../../auth/interfaces';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  storeSubscription: Subscription | undefined;
   urlBase = environment.restApiHost + environment.userImg;
   user: User | null = null;
   isAuthenticated: boolean = false;
@@ -21,7 +24,7 @@ export class HeaderComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.store.select(state => state.auth)
+    this.storeSubscription = this.store.select(state => state.auth)
       .subscribe(authState => {
         this.user = authState.user;
         this.isAuthenticated = authState.isAuthenticated;
@@ -30,5 +33,11 @@ export class HeaderComponent implements OnInit {
 
   onLogout() {
     this.store.dispatch(AuthActions.logout());
+  }
+
+  ngOnDestroy() {
+    if (this.storeSubscription) {
+      this.storeSubscription.unsubscribe();
+    }
   }
 }
