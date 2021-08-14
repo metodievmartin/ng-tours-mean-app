3089902d501a4c65e4233f00a6c711ffc4c9bf2f
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-14 19:51:58 +0300

    feat: add conditional rendering of the admin panel
    Connect side-nav to the store, display the admin navigational panel
    based on the user role.

--- src/app/users/components/side-nav/side-nav.component.ts
+++ src/app/users/components/side-nav/side-nav.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../../../reducers';
+import { User } from '../../../auth/interfaces';
+
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent implements OnInit {
+export class SideNavComponent implements OnInit, OnDestroy {
+  storeSubscription: Subscription | undefined;
+  user: User | null = null;
 
-  constructor() {
+  constructor(
+    public store: Store<fromApp.AppState>
+  ) {
   }
 
   ngOnInit(): void {
+    this.storeSubscription = this.store.select(state => state.auth)
+      .subscribe(authState => {
+        this.user = authState.user
+      });
   }
 
+  ngOnDestroy() {
+    if (this.storeSubscription) {
+      this.storeSubscription.unsubscribe();
+    }
+  }
 }
